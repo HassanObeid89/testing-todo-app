7b0a45ba6b6450db9e711972533b9b558856f6bd
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-09-28 16:15:28 +0200

    Save and load the data from local storage.

--- src/components/NormalScreen.jsx
+++ src/components/NormalScreen.jsx
@@ -4,7 +4,7 @@ import { listState } from "../state/listState";
 import ShoppingList from "./ShoppingList";
 import ButtonAddItem from './ButtonAddItem'
 export default function NormalScreen({ setModal }) {
-  const [list, setList] = useRecoilState(listState);
+  const [list] = useRecoilState(listState);
   return (
     <div>
       <ShoppingList list={list} />
