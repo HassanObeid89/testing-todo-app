1c0f2f9132b4e62e706d347ecbf19dc4682aa825
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2021-09-28 19:28:22 +0200

    Added the funtionality to mark item as completed.

--- src/components/NormalScreen.jsx
+++ src/components/NormalScreen.jsx
@@ -5,12 +5,24 @@ import ShoppingList from "./ShoppingList";
 import ButtonAddItem from './ButtonAddItem'
 import Sorter from "./Sorter";
 export default function NormalScreen({ setModal }) {
-  const [list] = useRecoilState(listState);
+  const [list,setList] = useRecoilState(listState);
+
+  const acquiredList = list.filter((item) => item.acquired === true);
+  const pendingList = list.filter((item) => item.acquired === false);
+
+  function editList(editedItem) {
+    const index = list.findIndex((item) => item.id === editedItem.id);
+    const clonedList = [...list];
+
+    clonedList[index] = editedItem;
+    setList(clonedList);
+  }
   return (
     <div>
       <Sorter/>
-      <ShoppingList list={list} />
+      <ShoppingList list={pendingList} editList={editList} />
       <ButtonAddItem setModal={setModal} />
+      <ShoppingList list={acquiredList} editList={editList}/>
     </div>
   );
 }
